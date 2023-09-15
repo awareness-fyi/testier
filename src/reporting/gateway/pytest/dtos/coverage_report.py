14b561dfc971214b1f813307ff12c60242c2fde7
podj <47007854+podj@users.noreply.github.com>
2023-09-15 16:49:30 +0300

    - introduce CI

--- src/reporting/gateway/pytest/dtos/coverage_report.py
+++ src/reporting/gateway/pytest/dtos/coverage_report.py
@@ -1,13 +1,13 @@
 from decimal import Decimal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from reporting.models.coverage_report import CoverageReport
 
 
 class PytestCoverageReport(BaseModel):
     class Totals(BaseModel):
-        percent_covered: Decimal
+        percent_covered: Decimal = Field(..., decimal_places=2)
         percent_covered_display: str
 
     totals: Totals
