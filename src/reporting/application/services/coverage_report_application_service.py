1dcf5dcd8caea282bceb8d1d1ec8d513b9789c6d
podj <47007854+podj@users.noreply.github.com>
2023-09-15 15:49:41 +0300

    - introduce CI

--- src/reporting/application/services/coverage_report_application_service.py
+++ src/reporting/application/services/coverage_report_application_service.py
@@ -16,14 +16,14 @@ class CoverageReportApplicationService:
         self._report_parser = PytestCoverageReportParser()
         self._notification_service = NotificationService()
 
-    def run(self, file_path: str, pull_request: str, repository: str):
+    def run(self, file_path: str, pull_request: str):
         raw = self._file_reader.read(Path(file_path))
         report = self._report_parser.parse(json.loads(raw))
         pull_request = self._github_service.get_pull_request(pull_request)
 
         main = self._github_service.get_main_branch()
         coverage_diff = main.coverage_report.compare(report)
-        self._github_service.upsert(pull_request.id, report, repository, coverage_diff)
+        self._github_service.upsert(pull_request.id, report, coverage_diff)
 
         message = f"Coverage change is {coverage_diff:.2f}%.\nFrom {main.coverage_report.percent:.2f}% to {report.percent:.2f}%"
         notification = self._notification_service.get(Channel.GITHUB)
