006303eae1471878c4a8d40d8a3f7f6b224362de
podj <47007854+podj@users.noreply.github.com>
2023-09-16 03:50:42 +0300

    more coverage

--- src/reporting/application/repositories/branch_repo.py
+++ src/reporting/application/repositories/branch_repo.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 
+from infrastructure.logger import logger
 from reporting.application.mappers.branch_mapper import BranchMapper
 from reporting.db.models.branch_document import BranchDocument
 from reporting.models.branch import Branch
@@ -21,6 +22,7 @@ class BranchRepo:
         return self._mapper.map_to_model(doc)
 
     def update_coverage(self, name: str, coverage_diff: Decimal) -> None:
-        BranchDocument.objects(name=name, repository=self._repository).update_one(
+        logger.info(f"updating branch '{name}' coverage with {coverage_diff}")
+        BranchDocument.objects(id=f"{self._repository}_{name}").update_one(
             upsert=True,
             inc__coverage_rate=coverage_diff)
