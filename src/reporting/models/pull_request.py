4dcc3371c9823ef53dccbd856d3584d4c6ecb36c
Itamar Podjarski <47007854+podj@users.noreply.github.com>
2023-09-30 16:51:08 +0300

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Rever…""

--- src/reporting/models/pull_request.py
+++ src/reporting/models/pull_request.py
@@ -6,7 +6,6 @@ from reporting.models.gituhub_user import GithubUser
 from reporting.models.repository import Repository
 
 
-
 class PullRequest(BaseModel):
     github_pull_request_number: str
     branch: Branch
