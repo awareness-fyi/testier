1d7b32b0777a145b3483a2e09182f652ce826374
podj <47007854+podj@users.noreply.github.com>
2024-01-26 15:04:37 +0200

    introduce 2 new services

---
+++ services/testier-playground/tests/test_calculator.py
@@ -0,0 +1,35 @@
+from decimal import Decimal
+
+from production_code.calculator import Calculator
+
+
+class TestCalculator:
+    def test_add(self):
+        calculator = Calculator()
+
+        assert calculator.add(1, 1) == 2
+
+    def test_subtract(self):
+        calculator = Calculator()
+
+        assert calculator.subtract(2, 1) == 1
+
+    def test_mod(self):
+        calculator = Calculator()
+
+        assert calculator.mod(20, 4) == 0
+
+    def test_divide(self):
+        calculator = Calculator()
+
+        assert calculator.divide(20, 4) == Decimal(5)
+
+    def test_pow(self):
+        calculator = Calculator()
+
+        assert calculator.pow(2, 3) == 8
+
+    def test_assembly(self):
+        calculator = Calculator()
+
+        calculator.assembly(1, 2)
--- test.sh
+++ test.sh
@@ -1,6 +1,19 @@
 COMMIT_SHA=$1
 
-pytest --cov-report json:cov.json --cov=testier-playground/production_code testier-playground/tests
 
-curl --fail-with-body -X POST "https://bb80-46-117-106-193.ngrok-free.app/v1/ci-runs?commit_sha=$COMMIT_SHA&project_id=backend" -H "Authorization: gAAAAABlmT83KgI1_5zaWIa6zBGLnZEU0rqtMq-MPyoTqAyJTUwBQ6DgkVfGDgM1ZnvxhXRJcEzsqrq-nDywiQYUfPMRFQfpMWgGXpVRiXychixn-nMakbCRg6T72j17FGCeJq-ej8R2" -F report=@./cov.json
-#curl --fail-with-body -X POST "https://bb80-46-117-106-193.ngrok-free.app/v1/ci-runs?commit_sha=$COMMIT_SHA&project_id=frontend" -H "Authorization: gAAAAABlmT83KgI1_5zaWIa6zBGLnZEU0rqtMq-MPyoTqAyJTUwBQ6DgkVfGDgM1ZnvxhXRJcEzsqrq-nDywiQYUfPMRFQfpMWgGXpVRiXychixn-nMakbCRg6T72j17FGCeJq-ej8R2" -F report=@./cov.json
+CHANGED_SERVICES=$(git diff ..main --name-only | xargs -L1 dirname | grep -vw . | sed 's/^services\///' | sed 's/\/.*$//' | sort | uniq)
+if [[ -n "${CHANGED_SERVICES}" ]]; then
+  cd services
+  for SERVICE in $CHANGED_SERVICES; do
+    if [ -d "$SERVICE" ]; then
+      echo "  *  Moving to the service '$SERVICE'"
+      cd "$SERVICE"
+      echo "  *  Building testing image for the $SERVICE service"
+      pytest --cov-report json:cov.json --cov=src tests
+      curl --fail-with-body -X POST "https://bb80-46-117-106-193.ngrok-free.app/v1/ci-runs?commit_sha=$COMMIT_SHA&project_id=$SERVICE" -H "Authorization: gAAAAABlmT83KgI1_5zaWIa6zBGLnZEU0rqtMq-MPyoTqAyJTUwBQ6DgkVfGDgM1ZnvxhXRJcEzsqrq-nDywiQYUfPMRFQfpMWgGXpVRiXychixn-nMakbCRg6T72j17FGCeJq-ej8R2" -F report=@./cov.json
+      echo "  *  Going back to the services directory"
+      cd ../
+    fi
+  done
+fi
+
