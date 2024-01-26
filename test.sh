619c1864f7bca8b4f21cd79ea8b2622383cc6f8d
podj <47007854+podj@users.noreply.github.com>
2024-01-26 19:43:21 +0200

    introduce 2 new services

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ if [[ -n "${CHANGED_SERVICES}" ]]; then
     PYTHONPATH=$TEMP_PYTHONPATH:$(pwd)/$SERVICE
     export PYTHONPATH
     pytest --cov-report json:cov.json --cov=src tests
-    curl --fail-with-body -X POST "https://bb80-46-117-106-193.ngrok-free.app/v1/ci-runs?commit_sha=$COMMIT_SHA&project_id=$SERVICE&project_glob=services/$SERVICE/*" -H "Authorization: gAAAAABlmT83KgI1_5zaWIa6zBGLnZEU0rqtMq-MPyoTqAyJTUwBQ6DgkVfGDgM1ZnvxhXRJcEzsqrq-nDywiQYUfPMRFQfpMWgGXpVRiXychixn-nMakbCRg6T72j17FGCeJq-ej8R2" -F report=@./cov.json
+    curl --fail-with-body -X POST "https://c2cc-46-117-106-193.ngrok-free.app/v1/ci-runs?commit_sha=$COMMIT_SHA&project_id=$SERVICE&project_glob=services/$SERVICE/*" -H "Authorization: gAAAAABlmT83KgI1_5zaWIa6zBGLnZEU0rqtMq-MPyoTqAyJTUwBQ6DgkVfGDgM1ZnvxhXRJcEzsqrq-nDywiQYUfPMRFQfpMWgGXpVRiXychixn-nMakbCRg6T72j17FGCeJq-ej8R2" -F report=@./cov.json
     echo "\nDone testing $SERVICE"
     cd ../
 
