bd55b6dcb9ab40fe9f49e777e783f98671e54aa1
podj <47007854+podj@users.noreply.github.com>
2024-01-26 15:05:33 +0200

    introduce 2 new services

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ if [[ -n "${CHANGED_SERVICES}" ]]; then
       cd "$SERVICE"
       echo "  *  Building testing image for the $SERVICE service"
       pytest --cov-report json:cov.json --cov=src tests
-      curl --fail-with-body -X POST "https://bb80-46-117-106-193.ngrok-free.app/v1/ci-runs?commit_sha=$COMMIT_SHA&project_id=$SERVICE" -H "Authorization: gAAAAABlmT83KgI1_5zaWIa6zBGLnZEU0rqtMq-MPyoTqAyJTUwBQ6DgkVfGDgM1ZnvxhXRJcEzsqrq-nDywiQYUfPMRFQfpMWgGXpVRiXychixn-nMakbCRg6T72j17FGCeJq-ej8R2" -F report=@./cov.json
+      curl --fail-with-body -X POST "https://bb80-46-117-106-193.ngrok-free.app/v1/ci-runs?commit_sha=$COMMIT_SHA&project_id=$SERVICE&project_glob=services/$SERVICE/*" -H "Authorization: gAAAAABlmT83KgI1_5zaWIa6zBGLnZEU0rqtMq-MPyoTqAyJTUwBQ6DgkVfGDgM1ZnvxhXRJcEzsqrq-nDywiQYUfPMRFQfpMWgGXpVRiXychixn-nMakbCRg6T72j17FGCeJq-ej8R2" -F report=@./cov.json
       echo "  *  Going back to the services directory"
       cd ../
     fi
